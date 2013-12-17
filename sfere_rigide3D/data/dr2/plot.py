b732c668229596fe8f31e938534540a1e71195ca
entangled90 <sana.carlo@gmail.com>
2013-12-17 17:38:08 +0100

    fixed 3d copiando da 2d

---
+++ sfere_rigide3D/data/dr2/plot.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from operator import itemgetter
+x=[]
+y=[]
+filename = sys.argv[1]
+#Carica il file fottendosene di "#"
+temp = np.loadtxt(filename, comments='#')
+#Ordina i valori x,y secondo la x:
+sorted(temp, key=itemgetter(0))
+for vector in temp:
+	x.append(float(vector[0]))
+	y.append(float(vector[1]))
+plt.plot(x,y,'b')
+plt.show()
